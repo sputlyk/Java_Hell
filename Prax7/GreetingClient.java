416a27f54adeb76d76209458a81ed0ecba6fa1b1
sputlyk <fsfarhanshaikh475@gmail.com>
2023-02-26 15:58:27 +0530

    test

--- Prax7/GreetingClient.java
+++ Prax7/GreetingClient.java
@@ -1,14 +1,13 @@
 import java.net.*;
 import java.io.*;
 
-
 public class GreetingClient {
-
 
    public static void main(String [] args) {
       String serverName = args[0];
       int port = Integer.parseInt(args[1]);
-      try {
+      try 
+      {
          System.out.println("Connecting to " + serverName + " on port " + port);
          Socket client = new Socket(serverName, port);
          
